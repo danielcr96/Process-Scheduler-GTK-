729e32b4ce8563b05b173baac205fa593ee64848
AndreyNavarro <30609534+AndreyNavarro@users.noreply.github.com>
2019-10-16 16:14:07 -0600

    Update tay.h

--- tay.h
+++ tay.h
@@ -13,30 +13,30 @@
 *  @arg x Variable dependiente de la función
 *  @arg n Cantidad de términos de la serie
 */
-double arcsin(double x, int n)
+double taylor(int n, double sumaAcumulada)
 {
     double fact,sum;
-    fact=sum=1.0;
+    fact=1.0;
+	sum=sumaAcumulada;
     int i;
-    for(i=1;i<=n;i++)
+	int x=1;
+    for(i=n;i<=n;i++)
     {
 	fact *= (2 * (double)i - 1)/(2 * (double)i );
 	double po = pow(x,(2*(double)i+1));
 	double factwo = po / (2*(double)i+1);
 	sum += fact * (pow(x,(2*(double)i+1)) / (2*(double)i+1));
     }
+
     return sum;
+
+
+	//printf("aproximación pi = %lf\n",2*arcsin(1.0,n*50));
 }
 /*
-
 void taylor (){
 	int n;
     printf("Cantidad de unidades de trabajo: ");
     scanf("%d",&n);
     printf("2 arcsin(1) = %lf\n",2*arcsin(1.0,n*50));
 }*/
-
-void taylor (int n){
-
-    printf("2 arcsin(1) = %lf\n",2*arcsin(1.0,n*50));
-}
